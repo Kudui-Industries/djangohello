105638271162bfdfb1225b96db70040f6949a4c9
Sztryms <lewandowskyszymon+git@gmail.com>
2025-05-17 12:27:20 +0200

    fixed disallowed host

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+import os
 from django.shortcuts import render
 
+ENV = os.environ.get('ENV', 'dev')
 
 def hello_world(request):
-    return HttpResponse("<h1>Hello, World! :test1 </h1>")
+    return HttpResponse(f"<h1>Hello, World!</h1><p>Środowisko: {ENV}</p><p>test aplikacyji </p>")
