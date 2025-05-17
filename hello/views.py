d5bccc7743cac3ef104aa88ae64c40ac85e7ea2d
Sztryms <lewandowskyszymon+git@gmail.com>
2025-05-17 12:32:14 +0200

    fixed wring variable name

--- hello/views.py
+++ hello/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 import os
 from django.shortcuts import render
 
-ENV = os.environ.get('ENV', 'dev')
+ENV = os.environ.get('env', 'dev')
 
 def hello_world(request):
     return HttpResponse(f"<h1>Hello, World!</h1><p>Środowisko: {ENV}</p><p>test aplikacyji </p>")
