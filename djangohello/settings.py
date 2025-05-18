3ecd1e2b8ff6dda79de3ed12205a489d6eb8ccf1
Sztryms <lewandowskyszymon+git@gmail.com>
2025-05-18 11:43:41 +0200

    checking if db connection works

--- djangohello/settings.py
+++ djangohello/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 
-ENV = os.environ.get('env', 'dev')
+ENV = os.environ.get('ENV', 'dev')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
