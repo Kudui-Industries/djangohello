f0a45851e32e307545892c0a4c8e2a4078fad4d4
Sztryms <lewandowskyszymon+git@gmail.com>
2025-05-28 23:23:45 +0200

    checking if db connection works

--- hello/urls.py
+++ hello/urls.py
@@ -7,7 +7,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('hello/', hello_world,name='hello_world'),
     path('ind/', index,name='index'),
-    path('', auth_views.LoginView.as_view(template_name= "magazyn/login.html"), name="login"),
+    path('', index,name='index'),
+    path('login/', auth_views.LoginView.as_view(template_name= "magazyn/login.html"), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name= "magazyn/logout.html"), name="logout"),
 
 
